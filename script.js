19b98b0213f2be1cce091cf8ac24c7b5c15bc647
technomad04 <agarwalsamiksha88@gmail.com>
2025-03-04 13:02:23 +0530

    Added the form on clicking the new button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myLibrary = [];
-let html = '';
+let html = '', htmlLoop = '';
 
 function Book(title, author){
     this.title = title;
@@ -13,11 +13,24 @@ function addBookToLibrary(title, author) {
 
 function display(){
     myLibrary.forEach((value, index)=>{
-        let htmlLoop = `<div>
-        <div><strong>TITLE: <\strong>${value.title}<\div>
-        <div><strong>AUTHOR: <\strong>${value.author}<\div>
-        <\div>`;
+        htmlLoop = `<div>
+        <div><strong>TITLE: <\strong>${value.title}</div>
+        <div><strong>AUTHOR: <\strong>${value.author}</div>
+        </div>`;
         html += htmlLoop;
     });
     document.querySelector('.container').innerHTML = html;
 }
+
+document.querySelector('.new').addEventListener('click', function(){
+    let htmlIn = `<div>
+        <label for="book-name"><strong>Title: </strong></label>
+        <input type="text" name="book-name" id="book-name">
+        <label for="book-author"><strong>Author: </strong></label>
+        <input type="text" name="book-author" id="book-author">
+        <button class="submit">Submit</button>
+    </div>`;
+    document.querySelector('.form').innerHTML = htmlIn;
+});
+
+let submit = document.querySelector('.submit');
