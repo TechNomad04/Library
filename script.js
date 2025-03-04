7b11001b15f3c5ae5c12b55754835f6d5284f66f
technomad04 <agarwalsamiksha88@gmail.com>
2025-03-04 10:23:28 +0530

    Added the function to display the books in the library

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let html = '';
 
 function Book(title, author){
     this.title = title;
@@ -8,4 +9,15 @@ function Book(title, author){
 function addBookToLibrary(title, author) {
     let book = new Book(title, author);
     myLibrary.push(book);
+}
+
+function display(){
+    myLibrary.forEach((value, index)=>{
+        let htmlLoop = `<div>
+        <div><strong>TITLE: <\strong>${value.title}<\div>
+        <div><strong>AUTHOR: <\strong>${value.author}<\div>
+        <\div>`;
+        html += htmlLoop;
+    });
+    document.querySelector('.container').innerHTML = html;
 }
